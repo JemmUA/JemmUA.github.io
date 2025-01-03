e89607f88f7ee79b917f15ead4116f8ba2c7f1d3
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-01-03 23:28:03 +0200

    tailwind

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        color__gift: "#ff5533",
+        color__tree: "#33ff33",
+        color__garland: "#3333ff",
+      },
+    },
+  },
+  plugins: [],
+}
+
