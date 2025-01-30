98674d8c2e628e54923b438940a954b823595b88
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-01-30 14:56:32 +0200

    addubg closures

---
+++ js_closures/main.js
@@ -0,0 +1,170 @@
+console.log('#5. JavaScript homework example file')
+
+/*
+ * #1
+ *
+ * Створіть функцію counter(), яка має реалізувати лічильник за допомогою замикання:
+ * функція може приймати число як аргумент counter(n)
+ * якщо число передано у функцію - лічба починається із зазначеного числа
+ * якщо ні - то лічба триває
+ */
+
+const counter = function() {
+  let counterValue = 0;
+  function incValue(n){
+    if (typeof(n) == 'number') {
+      return counterValue = n;
+    }
+      counterValue = counterValue + 1;
+    return counterValue;
+  }
+  return incValue;
+}();
+console.log("Task #1");
+console.log(counter()) // 0
+console.log(counter()) // 1
+console.log(counter(100)) // 100
+console.log(counter()) // 101
+console.log(counter()) // 102
+console.log(counter(500)) // 500
+console.log(counter()) // 501
+console.log(counter()) // 502
+console.log(counter(0)) // 0
+console.log(counter()) // 0
+console.log(counter()) // 1
+
+/*
+ * #2
+ *
+ * Створіть функцію counterFactory, яка має реалізувати три методи за допомогою замикання:
+ * початкове значення лічильника - 0
+ * counterFactory.value() - повертає значення лічильника
+ * counterFactory.value(n) - встановлює значення лічильника, повертає нове значення
+ * counterFactory.increment() - збільшує значення лічильника на 1
+ * counterFactory.decrement() - зменшує значення лічильника на 1
+ */
+
+const counterFactory = function () {
+  let counterValue = 0;
+  function valueIs (n) {
+    if (typeof(n) == 'number') {
+      return counterValue = n;
+    } else {
+      return counterValue;
+    }
+  }
+  function incValue() {
+    // console.log(counterValue + 1);
+    return counterValue = counterValue + 1;
+  }
+  function decValue() {
+    return counterValue = counterValue - 1;
+  }
+  const counterObject = {
+    value: valueIs,
+    increment: incValue,
+    decrement: decValue,
+  }
+  return counterObject;
+}();
+
+console.log("Task #2");
+console.log(counterFactory.value()) // 0
+counterFactory.increment()
+counterFactory.increment()
+counterFactory.increment()
+console.log(counterFactory.value()) // 3
+counterFactory.decrement()
+counterFactory.decrement()
+console.log(counterFactory.value()) // 1
+console.log(counterFactory.value(100)) // 100
+counterFactory.decrement()
+console.log(counterFactory.value()) // 99
+console.log(counterFactory.value(200)) // 200
+counterFactory.increment()
+console.log(counterFactory.value()) // 201
+
+/*
+ * #3
+ *
+ * Створіть функцію myPow(a, b, myPrint). Всередині реалізуйте рекурсію для підрахунку результату піднесення числа a до ступеня b.
+ * - Функція myPrint(a, b, res) - глобальна функція, що має генерувати з параметрів a, b, res рядок вигляду 'a^b=res' і повертати його.
+ * - myPrint() має бути передана в myPow() як параметр і викликана всередині як callback-функція.
+ * - функція myPow() як значення, що повертається, приймає результат myPrint().
+ * Наприклад:
+ * console.log(myPow(3, 4, myPrint)); // 3^4=81
+ * console.log(myPow(2, 3, myPrint)); // 2^3=8
+ * console.log(myPow(2, 0, myPrint))  // 2^0=1
+ * console.log(myPow(2, -2, myPrint)) // 2^-2=0.25
+ */
+
+// const myPrint = () => {}
+// const myPow = () => {}
+
+// console.log(myPow(3, 4, myPrint)) // 3^4=81
+// console.log(myPow(2, 3, myPrint)) // 2^3=8
+// console.log(myPow(2, 0, myPrint)) // 2^0=1
+// console.log(myPow(2, -2, myPrint)) // 2^-2=0.25
+
+
+/*
+ * #4
+ * Створіть функцію myMax(arr), яка як параметр приймає
+ * довільний числовий масив і повертає максимальне число з переданого їй масиву.
+ * У реалізації функції має бути застосовано метод Math.max() і apply().
+ */
+
+const list = [12, 23, 100, 34, 56, 9, 233]
+// const myMax = () => {}
+const myMax = (l) => {
+  let maxValue = l[0];
+  let maxOfTwo;
+  for (let i = 0; i < l.length - 1; i++) {
+    maxOfTwo = Math.max.apply(l, [l[i], l[i + 1]]);
+    maxValue = maxValue > maxOfTwo ? maxValue : maxValue = maxOfTwo;
+  }
+  return maxValue;
+}
+
+console.log("Task #4");
+console.log(myMax(list)); // 233
+
+/*
+ * #5
+ *
+ * Створіть функцію myMul(a, b), яка буде множити числа а і b, повертаючи результат.
+ */
+const myMul = (a, b) => a * b;
+/*
+ * Створіть функції myDouble(n), яка приймає один параметр і подвоює його.
+ * Використовувати множення або інші математичні операції всередині функції - заборонено, тільки bind() і myMul().
+ * Функція повертає результат обчислення.
+ */
+
+const multObj = { // створюю об'єкт, для використання bind(),
+  // і додаю в об'єкт поля з множниками, чому б і ні :))
+  multDouble : 2,
+  multTriple : 3
+}
+
+// const myDouble
+const myDouble = myMul.bind(multObj, multObj.multDouble);
+// const myTriple = myMul.bind(multObj, 2);
+
+console.log("Task #5");
+console.log("My doubles:");
+console.log(myDouble(3)) // = myMul(2, 3) = 6
+console.log(myDouble(4)) // = myMul(2, 4) = 8
+console.log(myDouble(5)) // = myMul(2, 5) = 10
+
+// Аналогічним чином створюємо функцію myTriple(n), яка потроює параметр, що приймає, повертаючи результат.
+
+// const myTriple
+const myTriple = myMul.bind(multObj, multObj.multTriple);
+
+console.log("My triples:");
+console.log(myTriple(3)) // = myMul(3, 3) = 9
+console.log(myTriple(4)) // = myMul(3, 4) = 12
+console.log(myTriple(5)) // = myMul(3, 5) = 15
+
+// export { counter, counterFactory, myPow, myMax, myMul, myDouble, myTriple }
