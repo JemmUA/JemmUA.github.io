74a91e2e7b33782086a053a957685ac869fd6bed
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-02-08 18:55:52 +0200

    JS OOP.Stairs, Votes

---
+++ js_oop/stairs.js
@@ -0,0 +1,30 @@
+// Це stairs (сходи) – об'єкт, який дозволяє підніматися вгору та спускатися (не замикання, просто обʼєкт):
+
+const stairs = {
+  step: 0,
+  up () {
+    this.step++;
+    return this;
+  },
+  down () {
+    this.step--;
+    return this;
+  },
+  showStep () { // показує поточну сходинку
+    console.log(this.step);
+    return this;
+  }
+};
+
+
+console.log("Task: \"Stairs\"")
+// Тепер, якщо нам потрібно зробити кілька послідовних викликів, ми можемо виконати це так:
+
+stairs.up();
+stairs.up();
+stairs.down();
+stairs.showStep(); // 1
+
+// Змініть код методів up, down і showStep таким Таким чином, щоб їх виклик можна було зробити по ланцюжку, наприклад:
+
+stairs.up().up().down().up().up().up().showStep(); // 5
---
+++ js_oop/votes.js
@@ -0,0 +1,86 @@
+/*
+  Можна зробити на замиканнях (closures) або на класах
+
+  Создать функцию, которая создает объект опросов - голосовалок.
+  Принимает массив опций для голосования. Возвращает объект.
+
+  Метод vote() принимает опцию. Если опция существует, поднимает ее счетчик на 1. Возвращает текущее количество голосов.
+  Если опции нет, возвращает текст Vote option Internet Explorer doesn't exist
+
+  Метод showVotes() выводит текущие результаты в виде опции и звездочек. Такой себе псевдо-график.
+  Звездочки должны начинаться с одной линии, чтобы можно было оценить, кто кого обогнал
+
+  Метод iterate принимает коллбек и выполняет его для каждой опции.
+  коллбек вызывается с двумя параметрами: название опции и количество голосов
+
+ */
+class Poll {
+  constructor(votes) {
+    this.votes = votes.map(vote => vote.toLowerCase());
+    this.counters = new Map();
+    this.votes.forEach((element) => this.counters.set(element, 0));
+  }
+  vote(option) {
+    if (option) option=option.toLowerCase();
+    else return "Wrong input. There is no option for vote";
+    if (option && this.votes.includes(option)) {
+      this.counters.set(option, this.counters.get(option) + 1);
+      return this.counters.get(option);
+    } else {
+      return `Vote option \"${option}\" doesn't exist`;
+    }
+  }
+  showVotes(){
+    const optionMaxLength = this.votes.reduce(((accumulator, value) => {
+      if (accumulator < value.length) {
+        accumulator = value.length;
+      }
+      return accumulator;
+    }), 0);
+    // const optionMaxLength = this.votes.reduce((acc, val) => acc < val.length ? val.length : acc, 0); // або так
+
+    this.votes.forEach((element, index) => {
+      console.log(element, " ".repeat(optionMaxLength - element.length), "*".repeat(this.counters.get(element)));
+    });
+  }
+
+}
+
+console.log("Task: \"Votes\"")
+
+const poll = new Poll(['chrome', 'firefox', 'OPERA', 'safari', 'edge']);
+
+// console.log(poll.vote(''));
+// console.log(poll.vote(' '));
+console.log(poll.vote('chrome'));
+console.log(poll.vote('chrome'));
+console.log(poll.vote('chrome'));
+console.log(poll.vote('CHROME'));
+console.log(poll.vote('Chrome'));
+console.log(poll.vote('firefox'));
+console.log(poll.vote('firefox'));
+console.log(poll.vote('FIREFOX'));
+console.log(poll.vote('opera'));
+console.log(poll.vote('edge'));
+console.log(poll.vote('Internet Explorer'));
+
+poll.showVotes();
+/*
+  chrome  *****
+  firefox ***
+  opera   *
+  safari
+  edge    *
+ */
+
+// poll.iterate((option, count) => {
+//   console.log(`${option} -> ${count}`);
+// });
+
+/*
+  chrome -> 5
+  firefox -> 3
+  opera -> 1
+  safari -> 0
+  edge -> 1
+ */
