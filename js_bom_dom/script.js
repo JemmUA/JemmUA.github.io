32dd6990ba6252b9d9ef992cc27d5de75acbcf68
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-02-08 11:12:36 +0200

    dom, bom: fix timer

--- js_bom_dom/script.js
+++ js_bom_dom/script.js
@@ -62,7 +62,7 @@ function setLocalStorageInfo(key, value, timeout) {
       localStorage.removeItem(key);
       console.log(`Видалено з Local Storage: ${key}=${value};`);
       // alert(`Параметр ${key}=${value} було видалено з Local Storage.`);
-    }, timeout);
+    }, timeout * 1000);
   }
 }
 
